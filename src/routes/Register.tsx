ebd1281dfa8358a72645c784516abb27415178a0
Cufee <vkouzin@outlook.com>
2023-10-30 15:04:52 -0500

    added form

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -1,75 +1,81 @@
-// username: string;
-// password: string;
-// passwordResetToken: string;
-// name: string;
-// surname: string;
-// companyName: string;
-
+import { useForm } from "react-hook-form";
 import useQuery from "../core/hooks/useQuery";
 
 function Register() {
   const query = useQuery();
   const invite = query.get("invite");
 
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+  console.log(watch("example"));
+
   return (
-    <form className="flex flex-col items-center justify-center w-full gap-2">
+    <form
+      className="flex flex-col items-center justify-center w-full gap-2"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <label htmlFor="email">
         <input
-          name="username"
           type="text"
           placeholder="Username"
           className="w-full max-w-md input input-bordered"
+          {...register("username", { required: true })}
         />
       </label>
       <label htmlFor="password">
         <input
-          name="password"
           type="password"
           placeholder="Password"
           className="w-full max-w-md input input-bordered"
+          {...register("password", { required: true })}
         />
       </label>
       <label htmlFor="passwordResetToken">
         <input
-          name="passwordResetToken"
           type="password"
           placeholder="Number of fingers you have"
           className="w-full max-w-md input input-bordered"
+          {...register("passwordResetToken", { required: true })}
         />
       </label>
       <label htmlFor="name">
         <input
-          name="name"
           type="text"
           placeholder="Name"
           className="w-full max-w-md input input-bordered"
+          {...register("name", { required: true })}
         />
       </label>
       <label htmlFor="surname">
         <input
-          name="surname"
           type="text"
           placeholder="Surname"
           className="w-full max-w-md input input-bordered"
+          {...register("surname", { required: true })}
         />
       </label>
       {(invite && (
         <label htmlFor="inviteToken">
           <input
             disabled
-            name="inviteToken"
             type="hidden"
             value={invite}
             className="w-full max-w-md input input-bordered"
+            {...register("inviteToken", { required: true })}
           />
         </label>
       )) || (
         <label htmlFor="companyName">
           <input
-            name="companyName"
             type="text"
             placeholder="Company Name"
             className="w-full max-w-md input input-bordered"
+            {...register("companyName", { required: true })}
           />
         </label>
       )}
